0e77c129b3f44d2785df588bb8ef3e84b4b06b12
Thiyam <Shafin.Thiyam@thomsonreuters.com>
2018-03-23 18:10:26 +0530

    First Commit

---
+++ src/Chap01/Welcome.java
@@ -0,0 +1,14 @@
+package Chap01;
+/**
+ * 
+ * @author Shafin Thiyam
+ *
+ */
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Wellcome Java!");
+
+	}
+
+}
